5adc8ddd143eaaed9aa3a7567b9095e41844c14a
RAJPUT6081 <bhadoriyashubham873@gmail.com>
2026-08-19 14:44:42 +0000

    altering and unsetting masking policy

--- Untitled 8.sql
+++ Untitled 8.sql
@@ -1,73 +1,50 @@
-USE DEMO_DB;
-USE ROLE ACCOUNTADMIN;
+ALTER TABLE IF EXISTS CUSTOMERS MODIFY COLUMN full_name 
+SET MASKING POLICY phone;
+ALTER TABLE IF EXISTS CUSTOMERS MODIFY COLUMN phone
+SET MASKING POLICY phone;
 
+use role analyst_masked
+select * from customers
 
--- Prepare table --
-create or replace table customers(
-  id number,
-  full_name varchar, 
-  email varchar,
-  phone varchar,
-  spent number,
-  create_date DATE DEFAULT CURRENT_DATE);
+SELECT * FROM table(information_schema.policy_references(policy_name=>'phone'));
 
--- insert values in table --
-insert into customers (id, full_name, email,phone,spent)
-values
-  (1,'Lewiss MacDwyer','lmacdwyer0@un.org','262-665-9168',140),
-  (2,'Ty Pettingall','tpettingall1@mayoclinic.com','734-987-7120',254),
-  (3,'Marlee Spadazzi','mspadazzi2@txnews.com','867-946-3659',120),
-  (4,'Heywood Tearney','htearney3@patch.com','563-853-8192',1230),
-  (5,'Odilia Seti','oseti4@globo.com','730-451-8637',143),
-  (6,'Meggie Washtell','mwashtell5@rediff.com','568-896-6138',600);
-
-
--- set up roles
-CREATE OR REPLACE ROLE ANALYST_MASKED;
-CREATE OR REPLACE ROLE ANALYST_FULL;
-
-
--- grant select on table to roles
-GRANT SELECT ON TABLE DEMO_DB.PUBLIC.CUSTOMERS TO ROLE ANALYST_MASKED;
-GRANT SELECT ON TABLE DEMO_DB.PUBLIC.CUSTOMERS TO ROLE ANALYST_FULL;
-
-GRANT USAGE ON SCHEMA DEMO_DB.PUBLIC TO ROLE ANALYST_MASKED;
-GRANT USAGE ON SCHEMA DEMO_DB.PUBLIC TO ROLE ANALYST_FULL;
-
--- grant warehouse access to roles
-GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ANALYST_MASKED;
-GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ANALYST_FULL;
-
-
--- assign roles to a user
-GRANT ROLE ANALYST_MASKED TO USER NIKOLAISCHULER;
-GRANT ROLE ANALYST_FULL TO USER NIKOLAISCHULER;
-
-
-
--- Set up masking policy
-
-create or replace masking policy phone 
-    as (val varchar) returns varchar ->
-            case        
-            when current_role() in ('ANALYST_FULL', 'ACCOUNTADMIN') then val
-            else '##-###-##'
-            end;
-  
-
--- Apply policy on a specific column 
 ALTER TABLE IF EXISTS CUSTOMERS MODIFY COLUMN phone 
-SET MASKING POLICY PHONE;
+UNSET MASKING POLICY;
+use role analyst_masked;
+select * from customers;
 
 
+create or replace masking policy names as (val varchar) returns varchar ->
+            case
+            when current_role() in ('ANALYST_FULL', 'ACCOUNTADMIN') then val
+            else CONCAT(LEFT(val,2),'*******')
+            end;
 
+alter table customers modify column full_name
+set masking policy names
 
--- Validating policies
+use role analyst_masked;
+select * from customers
 
-USE ROLE ANALYST_FULL;
-SELECT * FROM CUSTOMERS;
+create or replace masking policy phone as (val varchar) returns varchar ->
+            case
+            when current_role() in ('ANALYST_FULL', 'ACCOUNTADMIN') then val
+            else CONCAT(LEFT(val,2),'*******')
+            end;
+
+alter table customers modify column phone set masking policy phone
+
 
 USE ROLE ANALYST_MASKED;
 SELECT * FROM CUSTOMERS;
 
+use role accountadmin
 
+alter masking policy phone set body ->
+case        
+ when current_role() in ('ANALYST_FULL', 'ACCOUNTADMIN') then val
+ else '**-**-**'
+ end;
+
+USE ROLE ANALYST_MASKED;
+SELECT * FROM CUSTOMERS; 
