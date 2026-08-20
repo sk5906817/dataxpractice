ded738eeebb5e31adfae0add5cb290872da76abc
RAJPUT6081 <bhadoriyashubham873@gmail.com>
2026-08-20 05:56:24 +0000

    exercises dynamic data masking

--- Untitled 8.sql
+++ Untitled 8.sql
@@ -1,47 +1,63 @@
+USE DEMO_DB;
 USE ROLE ACCOUNTADMIN;
+ 
+-- Prepare table --
+create or replace table customers(
+  id number,
+  full_name varchar, 
+  email varchar,
+  phone varchar,
+  spent number,
+  create_date DATE DEFAULT CURRENT_DATE);
 
-create or replace masking policy emails as (val varchar) returns varchar ->
+  truncate table customers
+ 
+ 
+-- insert values in table --
+insert into customers (id, full_name, email,phone,spent)
+values
+  (1,'Lewiss MacDwyer','lmacdwyer0@un.org','262-665-9168',140),
+  (2,'Ty Pettingall','tpettingall1@mayoclinic.com','734-987-7120',254),
+  (3,'Marlee Spadazzi','mspadazzi2@txnews.com','867-946-3659',120),
+  (4,'Heywood Tearney','htearney3@patch.com','563-853-8192',1230),
+  (5,'Odilia Seti','oseti4@globo.com','730-451-8637',143),
+  (6,'Meggie Washtell','mwashtell5@rediff.com','568-896-6138',600);
+ 
+ 
+-- set up roles
+CREATE OR REPLACE ROLE ANALYST_MASKED;
+CREATE OR REPLACE ROLE ANALYST_FULL;
+ 
+-- grant select on table to roles
+GRANT SELECT ON TABLE DEMO_DB.PUBLIC.CUSTOMERS TO ROLE ANALYST_MASKED;
+GRANT SELECT ON TABLE DEMO_DB.PUBLIC.CUSTOMERS TO ROLE ANALYST_FULL;
+ 
+GRANT USAGE ON SCHEMA DEMO_DB.PUBLIC TO ROLE ANALYST_MASKED;
+GRANT USAGE ON SCHEMA DEMO_DB.PUBLIC TO ROLE ANALYST_FULL;
+ 
+-- grant warehouse access to roles
+GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ANALYST_MASKED;
+GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ANALYST_FULL;
+ 
+-- assign roles to a user
+GRANT ROLE ANALYST_MASKED TO USER RAJPUT6081;
+GRANT ROLE ANALYST_FULL TO USER RAJPUT6081;
+
+alter masking policy name set body->
 case
-  when current_role() in ('ANALYST_FULL') then val
-  when current_role() in ('ANALYST_MASKED') then regexp_replace(val,'.+\@','*****@') -- leave email domain unmasked
-  else '********'
-end;
-
-alter table customers modify column email set masking policy emails
-
-USE ROLE ANALYST_FULL;
-SELECT * FROM CUSTOMERS;
-
-USE ROLE ANALYST_MASKED;
-SELECT * FROM CUSTOMERS;
-
-USE ROLE ACCOUNTADMIN
-
-create or replace masking policy sha2 as (val varchar) returns varchar ->
-case 
-    when current_role() in ('ANALYST_FULL') then val
-    else sha2(val) --return hash of the column value
+    when current_role() in('ANALYST_FULL') then val
+    else CONCAT('***',LEFT(val,2))
 end
 
-alter table if exists customers modify column full_name 
-set masking policy sha2
+alter table customers modify column full_name
+unset masking policy 
 
-use role analyst_full;
+alter table customers modify column full_name
+set masking policy name 
+
+use role ANALYST_FULL;
 select * from customers;
 
 use role analyst_masked;
-select * from customers;
-
-use role accountadmin;
-alter table customers modify column email 
-unset masking policy
-
-create or replace masking policy dates as (val date) returns date ->
-case 
-    when current_role() in ('ANALYST_FULL') then val
-    else date_from_parts(0001,01,01)::date 
-end
-
-alter table customers modify column create_date
-set masking policy dates
+select * from customers
 
